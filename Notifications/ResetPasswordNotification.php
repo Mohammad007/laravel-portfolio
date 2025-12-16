e0add9c663712bb2cc6e2f1485cc14767c5f520a
mohitakki <38550218+mohitakki@users.noreply.github.com>
2025-12-16 17:58:42 +0530

    [Agent] Add a forgot password feature  - Notifications/ResetPasswordNotification.php

---
+++ Notifications/ResetPasswordNotification.php
@@ -0,0 +1,41 @@
+<?php
+// app/Notifications/ResetPasswordNotification.php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
+
+class ResetPasswordNotification extends ResetPasswordNotification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(string $token)
+    {
+        parent::__construct($token);
+        $this->onQueue('emails');
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Reset Your Password - ' . config('app.name'))
+            ->greeting('Hello!')
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', $url)
+            ->line('This password reset link will expire in ' . config('auth.passwords.users.expire') . ' minutes.')
+            ->line('If you did not request a password reset, no further action is required.')
+            ->salutation('Best regards,<br>' . config('app.name') . ' Team');
+    }
+}
